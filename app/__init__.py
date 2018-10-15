21ab384931f2eb45683965229c9baede6fd0b183
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-15 11:22:38 +0300

    wrote new index.html

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import Uploadset,configure_uploads,IMAGES 
+from flask_uploads import UploadSet,configure_uploads,IMAGES 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
