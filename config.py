f43361120cb72f846e3e036b8c0eef11f5331159
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-15 09:15:21 +0300

    macros.html

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
+    export MAIL_USERNAME=joeace2000@gmail.com
+    export MAIL_PASSWORD=killshot18
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:killshot18@joe/pitch'
 
 class ProdConfig:
--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-SQLALCHEMY_DATABASE_URI =postgresql+psycopg2://username:killshot18@joe/pitch
+export MAIL_USERNAME=joeace2000@gmail.com
+export MAIL_PASSWORD=killshot18
+
+python3.6 manage.py server 
